32846c2e9df37c8b48de1bda813805b40d81e429
Gogs <gogs@fake.local>
2025-04-14 17:29:10 +0500

    version 1

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,94 @@
-window.addEventListener("DOMContentLoaded", () => {
-  console.log("Loaded Scripts");
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', e => {
+      e.preventDefault();
+      document.querySelector(anchor.getAttribute('href')).scrollIntoView({ behavior: 'smooth' });
+    });
+  });
+
+  Fancybox.bind('[data-fancybox="plan"]', {});
+
+  if (document.querySelector(".swiper.main")) {
+    let mainSwiperThumbs = new Swiper(".swiper.main-thumbs", {
+      loop: true,
+      spaceBetween: 16,
+      slidesPerView: 5,
+      direction: "vertical",
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    });
+    let mainSwiper = new Swiper(".swiper.main", {
+      loop: true,
+      spaceBetween: 20,
+      thumbs: {
+        swiper: mainSwiperThumbs,
+      },
+    });
+  }
+
+  if (document.querySelector(".swiper.slider-plan")) {
+    let mainSwiper = new Swiper(".swiper.slider-plan", {
+      loop: true,
+      slidesPerView: 2,
+      spaceBetween: 60,
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    });
+  }
 });
+document.addEventListener('alpine:init', () => {
+  Alpine.data('main', () => ({
+    selectedTab: "description",
+    tabFixed: false,
+    screen: "",
+    tab_fixed() {
+      window.addEventListener('scroll', () => {
+        let topPos = this.$el.getBoundingClientRect().top;
+        if (this.screen >= 1024) {
+          if (topPos <= 110) {
+            this.tabFixed = true;
+          } else {
+            this.tabFixed = false;
+          }
+        } else {
+          if (topPos <= 70) {
+            this.tabFixed = true;
+          } else {
+            this.tabFixed = false;
+          }
+        }
+      });
+    }
+  }));
+
+  Alpine.data('textCollapse', () => ({
+    show: false,
+    text: "Показать полностью",
+    init() {
+      this.screen = document.documentElement.scrollWidth;
+      this.check();
+      window.addEventListener('resize', () => {
+        this.screen = document.documentElement.scrollWidth;
+        this.check();
+      });
+    },
+    change() {
+      this.show = !this.show;
+      if (this.show)
+        this.text = "Скрыть";
+      else
+        this.text = "Показать полностью";
+    },
+    check() {
+      if (this.screen >= 1024) {
+        this.show = true;
+      } else {
+        this.show = false;
+      }
+    }
+  }));
+})
--- tailwind.config.js
+++ tailwind.config.js
@@ -19,7 +19,45 @@ module.exports = {
   content: ["./src/**/*.{html,js,php}"],
   darkMode: "class",
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: '1.25rem',
+    },
+    screens: {
+      xs: '375px',
+      sm: '480px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1410px',
+    },
+    extend: {
+      fontFamily: {
+        "golos": ["Golos Text", "sans-serif"],
+      },
+      fontSize: {
+        "sm": ["12px", "16px"],
+        "1.5xl": ["1.375rem", "1.875rem"],
+        "2.5xl": ["1.75rem", "2.25rem"],
+        "3.5xl": ["2rem", "2.5rem"],
+      },
+      spacing: {
+        '7.5': '30px',
+        '15': '60px',
+      },
+      colors: {
+        'vsp-black-new': '#1F1F20',
+        'vsp-black': '#2E2E2E',
+        'vsp-red': '#D62D30',
+        'vsp-green': '#00AA00',
+        'vsp-light-gray': '#F2F6FF',
+        'vsp-f8-gray': '#F8F8F8',
+        'vsp-e-gray': '#EEEEEE',
+        'vsp-e9-gray': '#E9E9E9',
+        'vsp-gray': '#CACACA',
+        'vsp-gray-new': '#4C535E',
+        'vsp-dark-gray': '#2E2E2EB2',
+      },
+    },
   },
   plugins: plugins,
 };
